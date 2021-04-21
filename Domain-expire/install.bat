32e3aff9a83005fde4b9151c10372fe8a58fc517
jordanburch <jordan.burch@richweb.com>
2021-04-21 10:01:02 -0700

    Add files via upload

---
+++ Domain-expire/install.bat
@@ -0,0 +1,5 @@
+copy domain_expire.py "C:\Program Files (x86)\PRTG Network Monitor\Custom Sensors\python\"
+cd "C:\Program Files (x86)\PRTG Network Monitor\python\Scripts"
+pip.exe install python-whois
+echo "Domain Expire sensor is now installed"
+pause
