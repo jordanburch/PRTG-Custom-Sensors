2f6c1eb61508a6b8efb3bec8d85fd19f74a2ef88
jordanburch <jordan@yobo.dev>
2022-01-24 11:28:56 -0500

    Create install.bat

---
+++ mongo/install.bat
@@ -0,0 +1,5 @@
+copy mongosensor.py "C:\Program Files (x86)\PRTG Network Monitor\Custom Sensors\python\"
+cd "C:\Program Files (x86)\PRTG Network Monitor\python\Scripts"
+pip.exe install pymongo
+echo "mongo sensor is now installed"
+pause
